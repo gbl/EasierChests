39dc9dd29b06f12697e0e6db2b264dbedd11ea1b
Guntram Blohm <gbl@guntram.de>
2019-12-10 10:31:14 +0100

    update version and make ModMenu Config work

--- src/main/java/de/guntram/mcmod/easierchests/MMConfigurationHandler.java
+++ src/main/java/de/guntram/mcmod/easierchests/MMConfigurationHandler.java
@@ -1,5 +1,7 @@
 package de.guntram.mcmod.easierchests;
 
+import de.guntram.mcmod.fabrictools.ConfigurationProvider;
+import de.guntram.mcmod.fabrictools.GuiModOptions;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -14,7 +16,6 @@ public class MMConfigurationHandler implements ModMenuApi
 
     @Override
     public Optional<Supplier<Screen>> getConfigScreen(Screen screen) {
-        // return Optional.of(new GuiModOptions(screen, EasierChests.MODNAME, ConfigurationProvider.getHandler(EasierChests.MODNAME)));
-        return Optional.empty();
+        return Optional.of(new GuiModOptions(screen, EasierChests.MODNAME, ConfigurationProvider.getHandler(EasierChests.MODNAME)));
     }
 }
