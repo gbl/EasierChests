83642778f91c928fd0808ff3ab4e2a25b9985283
Guntram Blohm <gbl@guntram.de>
2020-01-15 21:43:06 +0100

    move to 1.15, make sort icon show up everywhere, make keys bindable, same item types sort by name, damage, number of enchantments, then enchantment types/levels now

--- src/main/java/de/guntram/mcmod/easierchests/EasierChests.java
+++ src/main/java/de/guntram/mcmod/easierchests/EasierChests.java
@@ -3,12 +3,24 @@ package de.guntram.mcmod.easierchests;
 import de.guntram.mcmod.fabrictools.ConfigurationProvider;
 import java.io.File;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
+import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_KP_1;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_KP_2;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_KP_7;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_KP_8;
 
 public class EasierChests implements ClientModInitializer 
 {
     static final String MODID="easierchests";
     static final String MODNAME="EasierChests";
-    static final String VERSION="@VERSION@";
+    static final String VERSION="1.15-fabric0.4.23-1.4";
+    
+    private static final String category = "key.categories.easierchests";
+    
+    public static FabricKeyBinding keySortChest, keyMoveToChest, keySortPlInv, keyMoveToPlInv;
     
     @Override
     public void onInitializeClient() {
@@ -17,5 +29,17 @@ public class EasierChests implements ClientModInitializer
         confHandler.load(ConfigurationProvider.getSuggestedFile(MODID));
         confHandler.load(null);
         FrozenSlotDatabase.init(new File("config"));
+        
+        KeyBindingRegistry.INSTANCE.addCategory(category);
+        keySortChest = registerKey("sortchest", GLFW_KEY_KP_7);
+        keyMoveToChest = registerKey("matchup", GLFW_KEY_KP_8);
+        keySortPlInv = registerKey("sortplayer", GLFW_KEY_KP_1);
+        keyMoveToPlInv = registerKey("matchdown", GLFW_KEY_KP_2);
+    }
+    
+    private FabricKeyBinding registerKey(String key, int code) {
+        FabricKeyBinding result = FabricKeyBinding.Builder.create(new Identifier("easierchests", key), InputUtil.Type.KEYSYM, code, category).build();
+        KeyBindingRegistry.INSTANCE.register(result);
+        return result;
     }
 }
