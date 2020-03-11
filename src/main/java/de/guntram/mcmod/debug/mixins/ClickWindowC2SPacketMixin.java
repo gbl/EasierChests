d5d3e55c8a7e6a32bcd12277a40920862debf3cd
Guntram Blohm <gbl@guntram.de>
2020-03-11 08:18:27 +0100

    Debugging uses logger not stdout

--- src/main/java/de/guntram/mcmod/debug/mixins/ClickWindowC2SPacketMixin.java
+++ src/main/java/de/guntram/mcmod/debug/mixins/ClickWindowC2SPacketMixin.java
@@ -3,6 +3,8 @@ package de.guntram.mcmod.debug.mixins;
 import net.minecraft.container.SlotActionType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.packet.ClickWindowC2SPacket;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,10 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClickWindowC2SPacket.class)
 public class ClickWindowC2SPacketMixin {
+    
+    static private final Logger LOGGER = LogManager.getLogger();
+
     @Inject(method="<init>(IIILnet/minecraft/container/SlotActionType;Lnet/minecraft/item/ItemStack;S)V", at=@At("RETURN"))
     private void dumpC2SNewInfo(int syncid, int slot, int button, SlotActionType actionType, ItemStack stack,
             short transaction, CallbackInfo ci) {
-        System.out.println("new ClickWindow C2S: syncid="+syncid+", slot="+slot+", button="+button+
+        LOGGER.debug(() -> "new ClickWindow C2S: syncid="+syncid+", slot="+slot+", button="+button+
                 ", action="+actionType.toString()+", item="+stack.getCount()+" of "+stack.getName().asString()+
                 ", transaction="+transaction);
     }
--- src/main/java/de/guntram/mcmod/debug/mixins/GuiActionConfirmDebugMixin.java
+++ src/main/java/de/guntram/mcmod/debug/mixins/GuiActionConfirmDebugMixin.java
@@ -3,6 +3,8 @@ package de.guntram.mcmod.debug.mixins;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.packet.ConfirmGuiActionS2CPacket;
 import net.minecraft.client.network.packet.InventoryS2CPacket;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,12 +12,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class GuiActionConfirmDebugMixin {
-    @Inject(method="onGuiActionConfirm", at=@At("HEAD"))
+    
+    static private final Logger LOGGER = LogManager.getLogger();
+
+    @Inject(method="onGuiActionConfirm", at=@At(value="INVOKE", target="Lnet/minecraft/client/network/packet/ConfirmGuiActionS2CPacket;getId()I"))
     private void dumpActionConfirmInfo(ConfirmGuiActionS2CPacket packet, CallbackInfo ci) {
-        System.out.println("confirm: id="+packet.getId()+", action="+packet.getActionId()+", accepted="+packet.wasAccepted());
+        LOGGER.debug(() -> "confirm: id="+packet.getId()+", action="+packet.getActionId()+", accepted="+packet.wasAccepted());
     }
     @Inject(method="onInventory", at=@At("HEAD"))
     private void dumpInventoryInfo(InventoryS2CPacket packet, CallbackInfo ci) {
-        System.out.println("inventory: guiid="+packet.getGuiId()+", slotcount="+packet.getSlotStacks().size());
+        LOGGER.debug(() -> "inventory: guiid="+packet.getGuiId()+", slotcount="+packet.getSlotStacks().size());
     }
 }
