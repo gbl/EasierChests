8854ce7a975bc286d22ea93742e2859096bf39b8
Guntram Blohm <gbl@guntram.de>
2020-06-20 12:01:26 +0200

    ModMenu deprecated old method

--- src/main/java/de/guntram/mcmod/easierchests/MMConfigurationHandler.java
+++ src/main/java/de/guntram/mcmod/easierchests/MMConfigurationHandler.java
@@ -2,6 +2,7 @@ package de.guntram.mcmod.easierchests;
 
 import de.guntram.mcmod.fabrictools.ConfigurationProvider;
 import de.guntram.mcmod.fabrictools.GuiModOptions;
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -18,4 +19,10 @@ public class MMConfigurationHandler implements ModMenuApi
     public Optional<Supplier<Screen>> getConfigScreen(Screen screen) {
         return Optional.of(new GuiModOptions(screen, EasierChests.MODNAME, ConfigurationProvider.getHandler(EasierChests.MODNAME)));
     }
+    
+    @Override
+    public ConfigScreenFactory getModConfigScreenFactory() {
+        return screen -> new GuiModOptions(screen, EasierChests.MODNAME, ConfigurationProvider.getHandler(EasierChests.MODNAME));
+    }
+
 }
